3a94835a84aefe41248972ab439af65f08846031
NhatBui <nhat_bui@epam.com>
2021-09-13 23:02:58 +0700

    init home

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "hello world";
+    }
+}
